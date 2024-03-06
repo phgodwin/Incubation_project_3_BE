5f047fa519a04ecb719b1ac31d31dbe5ed222bc1
liammcnabola <liam.mcnabola@protonmail.com>
2024-03-06 16:35:00 +0000

    started testing Items

--- src/test/java/com/LBG/legacy/selenium/ProjectTest.java
+++ src/test/java/com/LBG/legacy/selenium/ProjectTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,5 +40,44 @@ public class ProjectTest {
 
 	void testLogin() {
 		this.driver.get("http://localhost:" + this.port);
+		WebElement clickUserName = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div > div > div:nth-child(4) > label:nth-child(2) > input[type=text]"));
+		clickUserName.sendKeys("Liam");
+		WebElement clickPassword = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div > div > div:nth-child(4) > label:nth-child(4) > input[type=password]"));
+		clickPassword.sendKeys("Password");
+		WebElement clickLogin = this.driver
+				.findElement(By.cssSelector("#root > div > div > div > div > div:nth-child(4) > button"));
+		clickLogin.click();
+	}
+
+	@Test
+	@Order(2)
+
+	void testItem() {
+		this.driver.get("http://localhost:" + this.port);
+		WebElement clickInventory = this.driver
+				.findElement(By.cssSelector("#navbarNav > ul > li:nth-child(2) > a > b"));
+		clickInventory.click();
+		WebElement enterItemName = this.driver.findElement(
+				By.cssSelector("#root > div > div > div:nth-child(1) > form > label:nth-child(2) > input[type=text]"));
+		enterItemName.sendKeys("Tissues");
+		WebElement enterItemPrice = this.driver.findElement(
+				By.cssSelector("#root > div > div > div:nth-child(1) > form > label:nth-child(3) > input[type=text]"));
+		enterItemPrice.sendKeys("0.99");
+		WebElement enterItemQuantity = this.driver.findElement(
+				By.cssSelector("#root > div > div > div:nth-child(1) > form > label:nth-child(4) > input[type=text]"));
+		enterItemQuantity.sendKeys("1");
+		WebElement createItemButton = this.driver
+				.findElement(By.cssSelector("#root > div > div > div:nth-child(1) > form > button"));
+		createItemButton.click();
+		driver.navigate().refresh();
+	}
+
+	@Test
+	@Order(3)
+
+	void testCart() {
+
 	}
 }
