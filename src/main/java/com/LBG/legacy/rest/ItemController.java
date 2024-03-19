0cbe383900527af6a8378a45c8f4db180f123c79
unknown <lucy.yates@ntlworld.com>
2024-03-19 14:55:27 +0000

    remove item from cart

--- src/main/java/com/LBG/legacy/rest/ItemController.java
+++ src/main/java/com/LBG/legacy/rest/ItemController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.LBG.legacy.domain.Item;
 import com.LBG.legacy.services.ItemService;
 
-
 @RestController
 @CrossOrigin
 @RequestMapping("/item")
@@ -48,6 +47,11 @@ public class ItemController {
 	@PatchMapping("/update/{id}")
 	public ResponseEntity<Item> updateItem(@PathVariable int id, @RequestBody Item newItem) {
 		return this.service.updateItem(id, newItem);
+	}
+
+	@PatchMapping("/updateCartItem/{id}")
+	public ResponseEntity<Item> removeCartItem(@PathVariable int id) {
+		return this.service.removeCartItem(id);
 	}
 
 	@DeleteMapping("/remove/{id}")
