294d92afb7378b44e7ef02c560bd84a63c3bdc94
unknown <lucy.yates@ntlworld.com>
2024-03-25 16:35:05 +0000

    testing done

--- src/test/java/com/LBG/legacy/selenium/ProjectTest.java
+++ src/test/java/com/LBG/legacy/selenium/ProjectTest.java
@@ -1,6 +1,7 @@
 package com.LBG.legacy.selenium;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -77,6 +79,7 @@ public class ProjectTest {
 		WebElement clickInventory = this.driver
 				.findElement(By.cssSelector("#navbarNav > ul > li:nth-child(2) > a > b"));
 		clickInventory.click();
+		// adds item
 		WebElement enterItemName = this.driver.findElement(
 				By.cssSelector("#root > div > div > div:nth-child(1) > form > label:nth-child(2) > input[type=text]"));
 		enterItemName.sendKeys("T");
@@ -100,16 +103,7 @@ public class ProjectTest {
 		String alertMessage = alert.getText();
 		assertEquals("Item created successfully", alertMessage);
 		alert.accept();
-
-	}
-
-	@Test
-	@Order(3)
-	void testEditItem() throws InterruptedException {
-		this.driver.get("http://localhost:" + this.port);
-		WebElement clickInventory = this.driver
-				.findElement(By.cssSelector("#navbarNav > ul > li:nth-child(2) > a > b"));
-		clickInventory.click();
+		// edits item
 		WebElement editItemButton = this.driver.findElement(By.cssSelector(
 				"#root > div > div > div.container.mt-4 > div > div:nth-child(3) > div > div > ul > li:nth-child(2) > button"));
 		editItemButton.click();
@@ -132,32 +126,31 @@ public class ProjectTest {
 
 		WebElement editedItem = this.driver.findElement(
 				By.cssSelector("#root > div > div > div.container.mt-4 > div > div:nth-child(3) > div > div > h5"));
-		Assertions.assertEquals("18: Guitar lessons", editedItem.getText());
-
-	}
-
-	@Test
-	@Order(4)
-	void testDeleteItem() throws InterruptedException {
-		this.driver.get("http://localhost:" + this.port);
-		WebElement clickInventory = this.driver
-				.findElement(By.cssSelector("#navbarNav > ul > li:nth-child(2) > a > b"));
-		clickInventory.click();
+		Assertions.assertEquals("31: Guitar lessons", editedItem.getText());
+		// deletes item
 		WebElement deleteItemButton = this.driver.findElement(By.cssSelector(
 				"#root > div > div > div.container.mt-4 > div > div:nth-child(3) > div > div > ul > li:nth-child(3) > button"));
+		WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait1.until(ExpectedConditions.elementToBeClickable(deleteItemButton));
 		deleteItemButton.click();
 		Thread.sleep(500);
-		WebElement someElementAfterDelete = this.driver.findElement(
-				By.cssSelector("#root > div > div > div.container.mt-4 > div > div:nth-child(3) > div > div"));
-		assertTrue(someElementAfterDelete.isDisplayed(), "the element is not displayed after delete");
 
+		WebElement someElementAfterDelete;
+		try {
+			someElementAfterDelete = wait.until(ExpectedConditions.visibilityOfElementLocated(
+					By.cssSelector("#root > div > div > div.container.mt-4 > div > div:nth-child(3) > div > div")));
+			assertFalse(someElementAfterDelete.isDisplayed(), "Element still displayed after deletion");
+		} catch (TimeoutException e) {
+			assertTrue(true, "Element not found after deletion"); // Element not found after deletion
+		}
 	}
 
 	@Test
-	@Order(5)
+	@Order(3)
 
 	void testCart() throws InterruptedException {
 		this.driver.get("http://localhost:" + this.port);
+
 		WebElement clickOrders = this.driver.findElement(By.cssSelector("#basic-nav-dropdown > span"));
 		clickOrders.click();
 
@@ -165,21 +158,125 @@ public class ProjectTest {
 				.findElement(By.cssSelector("#navbarNav > ul > div > div > a:nth-child(1)"));
 		clickCurrentOrders.click();
 
+		// adds cart
 		WebElement enterCustomer = this.driver.findElement(
 				By.cssSelector("#root > div > div > div:nth-child(1) > form:nth-child(1) > label > input[type=text]"));
 		enterCustomer.sendKeys("Lucy");
-
 		WebElement createCartButton = this.driver
 				.findElement(By.cssSelector("#root > div > div > div:nth-child(1) > form:nth-child(1) > button"));
 		createCartButton.click();
+
 		Thread.sleep(500);
 
-		Alert cartAlert = driver.switchTo().alert();
-		String cartAlertMessage = cartAlert.getText();
-		assertEquals("Cart created successfully", cartAlertMessage);
-		cartAlert.accept();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		Alert alert = wait.withTimeout(Duration.ofSeconds(10)).until(ExpectedConditions.alertIsPresent());
+
+		String alertMessage = alert.getText();
+		assertEquals("Cart created successfully", alertMessage);
+		alert.accept();
+
+		// Edit customer name
+		WebElement editCustomer = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div.container.mt-4 > div > div:nth-child(2) > div > div > ul > li:nth-child(2) > button.btn.btn-warning"));
+		editCustomer.click();
+		Thread.sleep(500);
+		Alert alert1 = driver.switchTo().alert();
+		alert1.sendKeys("Lucy Yates");
+		alert1.accept();
+		Thread.sleep(500);
+
+		WebElement editedBuyer = this.driver.findElement(
+				By.cssSelector("#root > div > div > div.container.mt-4 > div > div:nth-child(2) > div > div > h5"));
+		Assertions.assertEquals("18: Lucy Yates", editedBuyer.getText());
+		Thread.sleep(500);
+		// adds item to cart
+		WebElement clickSelectItem = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div:nth-child(1) > form:nth-child(2) > label:nth-child(2) > select > option:nth-child(3)"));
+		clickSelectItem.click();
+		WebElement clickSelectCustomer = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div:nth-child(1) > form:nth-child(2) > label:nth-child(3) > select > option:nth-child(2)"));
+		clickSelectCustomer.click();
+
+		WebElement addToCartButton = this.driver
+				.findElement(By.cssSelector("#root > div > div > div:nth-child(1) > form:nth-child(2) > button"));
+		addToCartButton.click();
+		Thread.sleep(500);
+
+		Alert addAlert = driver.switchTo().alert();
+		String addAlertMessage = addAlert.getText();
+		assertEquals("Item added to cart successfully", addAlertMessage);
+		addAlert.accept();
+		Thread.sleep(500);
+		// Remove item from cart
+		WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement removeFromCart = wait1.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
+				"#root > div > div > div.container.mt-4 > div > div:nth-child(1) > div > div > ul > li:nth-child(3) > button")));
+		removeFromCart.click();
+
+		// Verify item deletion
+		try {
+			// Wait for element to be invisible after deletion
+			wait1.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(
+					"#root > div > div > div.container.mt-4 > div > div:nth-child(1) > div > div > ul > li:nth-child(3)")));
+			assertTrue(true, "Element not found after deletion");
+		} catch (TimeoutException e) {
+			assertFalse(true, "Element still displayed after deletion");
+		}
+
+		Thread.sleep(500);
+		WebElement deleteBuyer = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div.container.mt-4 > div > div:nth-child(2) > div > div > ul > li:nth-child(2) > button.btn.btn-danger"));
+		deleteBuyer.click();
+
+		try {
+			// Wait for element to be invisible after deletion
+			wait1.until(ExpectedConditions.invisibilityOfElementLocated(
+					By.cssSelector("#root > div > div > div.container.mt-4 > div > div:nth-child(2) > div > div")));
+			assertTrue(true, "Element not found after deletion");
+		} catch (TimeoutException e) {
+			assertFalse(true, "Element still displayed after deletion");
+		}
+		Thread.sleep(500);
+		WebElement totalButton = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div.container.mt-4 > div > div > div > div > ul > li:nth-child(4) > button"));
+		totalButton.click();
+
+		Thread.sleep(500);
+
+		Alert totalAlert = driver.switchTo().alert();
+		String totalAlertMessage = totalAlert.getText();
+		assertEquals("Total for Purbrook Park School's cart: £121.18", totalAlertMessage);
+		totalAlert.accept();
+		Thread.sleep(500);
+
+		WebElement completeButton = this.driver.findElement(By.cssSelector(
+				"#root > div > div > div.container.mt-4 > div > div > div > div > ul > li:nth-child(5) > button"));
+		completeButton.click();
+
+		Thread.sleep(500);
+
+		WebDriverWait wait11 = new WebDriverWait(driver, Duration.ofSeconds(10));
+		Alert alert11 = wait11.withTimeout(Duration.ofSeconds(10)).until(ExpectedConditions.alertIsPresent());
+
+		String alertMessage1 = alert11.getText();
+		assertEquals("Order completed successfully!", alertMessage1);
+		alert11.accept();
+
+		WebElement clickOrdersAgain = this.driver.findElement(By.cssSelector("#basic-nav-dropdown > span"));
+		clickOrdersAgain.click();
+
+		WebElement clickPreviousOrders = this.driver
+				.findElement(By.cssSelector("#navbarNav > ul > div > div > a:nth-child(2)"));
+		clickPreviousOrders.click();
+
+		Thread.sleep(500);
+
+		WebElement previousOrder = this.driver
+				.findElement(By.cssSelector("#root > div > div > div > div > div > div > div > h5"));
+		Assertions.assertEquals("3: Purbrook Park School", previousOrder.getText());
 
 	}
+
 //
 //	@Test
 //	@Order(5)
